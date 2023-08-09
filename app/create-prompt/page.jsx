1a11e0bc61972577dd2ae13783eb504f94caf248
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-08-09 11:57:48 +0200

    changed text

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -39,7 +39,7 @@ const CreatePrompt = () => {
 
   return (
     <Form
-      type='Create'
+      type='Add'
       post={post}
       setPost={setPost}
       submitting={submitting}
--- components/Form.jsx
+++ components/Form.jsx
@@ -4,7 +4,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className='w-full max-w-full flex-start flex-col'>
       <h1 className='head_text text-left'>
-        <span className='blue_gradient'>{type} Post</span>
+        <span className='blue_gradient'>{type} Thought</span>
       </h1>
       <p className='desc text-left max-w-md'>
         {type} and share amazing prompts with the world, and let your
@@ -17,7 +17,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
       >
         <label>
           <span className='font-satoshi font-semibold text-base text-gray-700'>
-            Your AI Prompt
+            Your thought
           </span>
 
           <textarea
